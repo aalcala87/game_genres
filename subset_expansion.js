2a9f6a0e381acb8a5b0b2dd5d9e3a41adca87559
aalcala87 <145299718+aalcala87@users.noreply.github.com>
2023-11-15 20:19:11 -0800

    Add files via upload

---
+++ subset_expansion.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$(document).ready(function () {
+    function handleClick(event) {
+        event.preventDefault();
+        const clickedLink = $(event.target);
+        const divAboveLink = clickedLink.prev();
+
+        //Toggle the hide class on the div element
+        divAboveLink.toggleClass('hide');
+        //See if div element above current link has class of hide
+        const isHidden = divAboveLink.hasClass('hide');
+        //Update link text based on the hide class
+        if (isHidden) {
+            clickedLink.text('Show more');
+        } else {
+            clickedLink.text('Show less');
+        }
+    }
+    $('a').click(handleClick);
+});
